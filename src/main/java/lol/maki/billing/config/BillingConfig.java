055729adeb0db65e0cbe1e7fecc849445d0cdd6b
Toshiaki Maki <makingx@gmail.com>
2026-03-12 21:15:49 +0900

    Tweak config

--- src/main/java/lol/maki/billing/config/BillingConfig.java
+++ src/main/java/lol/maki/billing/config/BillingConfig.java
@@ -7,6 +7,8 @@ import lol.maki.billing.model.Usage;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
@@ -17,68 +19,62 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.RecordFieldSetMapper;
-import org.springframework.boot.context.properties.bind.Bindable;
-import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BillingConfig {
-	private final Resource usageResource;
 
-	public BillingConfig(Environment environment) {
-		this.usageResource = Binder.get(environment)
-				.bind("usage.file.name", Bindable.of(Resource.class))
-				.orElseGet(() -> new ClassPathResource("usageinfo.csv"));
-	}
+    @Bean
+    public ItemProcessor<Usage, Bill> billItemProcessor() {
+        return new ItemProcessor<Usage, Bill>() {
+            @Override
+            public Bill process(Usage usage) throws Exception {
+                return Bill.fromUsage(usage);
+            }
+        };
+    }
 
-	@Bean
-	public Job billingJob(ItemReader<Usage> itemReader, ItemProcessor<Usage, Bill> itemProcessor, ItemWriter<Bill> itemWriter, JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-		final Step step = new StepBuilder("BilliProcessing", jobRepository)
-				.<Usage, Bill>chunk(3, transactionManager)
-				.reader(itemReader)
-				.processor(itemProcessor)
-				.writer(itemWriter)
-				.build();
-		return new JobBuilder("BillingJob", jobRepository)
-				.incrementer(new RunIdIncrementer())
-				.start(step)
-				.build();
-	}
+    @Bean
+    public ItemWriter<Bill> jdbcBillWriter(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Bill>()
+                .beanMapped()
+                .dataSource(dataSource)
+                .sql("INSERT INTO BILL_STATEMENTS (id, first_name, last_name, minutes, data_usage,bill_amount) VALUES (:id, :firstName, :lastName, :minutes, :dataUsage, :billAmount)")
+                .build();
+    }
 
-	@Bean
-	public ItemReader<Usage> usageItemReader() {
-		return new FlatFileItemReaderBuilder<Usage>()
-				.name("UsageItemReader")
-				.resource(this.usageResource)
-				.delimited()
-				.names("id", "firstName", "lastName", "minutes", "dataUsage")
-				.fieldSetMapper(new RecordFieldSetMapper<>(Usage.class))
-				.linesToSkip(1)
-				.build();
-	}
+    @Bean
+    public Step billiProcessingStep(ItemReader<Usage> itemReader, ItemProcessor<Usage, Bill> itemProcessor, ItemWriter<Bill> itemWriter, JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("BilliProcessing", jobRepository)
+                .<Usage, Bill>chunk(3, transactionManager)
+                .reader(itemReader)
+                .processor(itemProcessor)
+                .writer(itemWriter)
+                .build();
+    }
 
-	@Bean
-	public ItemProcessor<Usage, Bill> billItemProcessor() {
-		return new ItemProcessor<Usage, Bill>() {
-			@Override
-			public Bill process(Usage usage) throws Exception {
-				return Bill.fromUsage(usage);
-			}
-		};
-	}
+    @Bean
+    public Job billingJob(Step billiProcessingStep, JobRepository jobRepository) {
+        return new JobBuilder("BillingJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(billiProcessingStep)
+                .build();
+    }
 
-	@Bean
-	public ItemWriter<Bill> jdbcBillWriter(DataSource dataSource) {
-		return new JdbcBatchItemWriterBuilder<Bill>()
-				.beanMapped()
-				.dataSource(dataSource)
-				.sql("INSERT INTO BILL_STATEMENTS (id, first_name, last_name, minutes, data_usage,bill_amount) VALUES (:id, :firstName, :lastName, :minutes, :dataUsage, :billAmount)")
-				.build();
-	}
+    @Bean
+    public ItemReader<Usage> usageItemReader(@Value("${usage.file.name:classpath:usageinfo.csv}") Resource usageResource) {
+        return new FlatFileItemReaderBuilder<Usage>()
+                .name("UsageItemReader")
+                .resource(usageResource)
+                .delimited()
+                .names("id", "firstName", "lastName", "minutes", "dataUsage")
+                .fieldSetMapper(new RecordFieldSetMapper<>(Usage.class))
+                .linesToSkip(1)
+                .build();
+    }
 
 }
